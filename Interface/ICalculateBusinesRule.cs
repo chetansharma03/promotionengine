7dc6e6b0912d13dccaac63862f37f592e6dd7759
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 14:38:22 +0530

    interface to apply business rules

---
+++ Interface/ICalculateBusinesRule.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Entities;
+using PromotionEngine.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Interface
+{
+    public interface ICalculateBusinessRule
+    {
+        bool ValidateOrder(List<OrderItem> orderItems, Promotion promotion);
+        AnalizeOrderItems ApplyBusinessRules(List<OrderItem> orderItems, Promotion promotion);
+    }
+}
