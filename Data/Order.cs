d34c7ea856d4155647b78a0fc08d76e199eef5d5
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 08:46:56 +0530

    added order class, which is colllection of orderitem class

---
+++ Data/Order.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Entities;
+using System.Collections.Generic;
+
+namespace PromotionEngine.Data
+{
+    public class Order
+    {
+        public Order()
+        {
+            Items = new List<OrderItem>();
+        }
+        public List<OrderItem> Items { get; set; }
+    }
+}
