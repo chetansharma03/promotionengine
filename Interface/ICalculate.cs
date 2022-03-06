72fd6a8e61126b869170d510f376c187569bc63c
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 21:00:20 +0530

    order calculation using checkout summary

---
+++ Interface/ICalculate.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Entities;
+using PromotionEngine.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Interface
+{
+    public interface ICalculate
+    {
+        CheckoutSummary CalcualteOrder(Order order, List<Promotion> promotions);
+    }
+}
---
+++ Interface/ICalculatorType.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Entities;
+using PromotionEngine.Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Interface
+{
+    public interface ICalculatorType
+    {
+        CalculateBase GetCalculatorType(Promotion promotion);
+        DefaultBase GetDefaultCalculator(List<Promotion> promotions);
+    }
+}
