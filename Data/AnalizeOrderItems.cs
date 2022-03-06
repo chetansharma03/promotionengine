0395512eeab00a6e6af77e7d104c2e2eae3b30e5
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 14:36:57 +0530

    analyzing order items

---
+++ Data/AnalizeOrderItems.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Entities;
+using System.Collections.Generic;
+
+namespace PromotionEngine.Data
+{
+    public class AnalizeOrderItems
+    {
+        public AnalizeOrderItems()
+        {
+            ItemForProccessing = new List<OrderItem>();
+            SingleItems = new List<SingleItem>();
+        }
+        public int BundleCount { get; set; }
+        public int BundleItemModulus { get; set; }
+        public int BundleItemCount { get; set; }
+        public List<OrderItem> ItemForProccessing { get; set; }
+        public List<SingleItem> SingleItems { get; set; }
+    }
+}
