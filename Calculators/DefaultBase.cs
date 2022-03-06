8fbdcc6c1070a9f9e4306ba018ff5fa379d0ca28
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 14:33:55 +0530

    list of default promotions

---
+++ Calculators/DefaultBase.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Entities;
+using PromotionEngine.Calculators;
+using System.Collections.Generic;
+
+namespace PromotionEngine.Calculators
+{
+    public abstract class DefaultBase : CalculateBase
+    {
+        public List<Promotion> Promotions { get; set; }
+
+        public DefaultBase(List<Promotion> promotions)
+        {
+            Promotions = promotions;
+        }
+    }
+}
