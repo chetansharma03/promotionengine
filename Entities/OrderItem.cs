baa9365c0a57f41a703a7814d90b5b9ccbe53c1d
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 07:42:07 +0530

    modified version of orderitem class, added data to display

--- Entities/OrderItem.cs
+++ Entities/OrderItem.cs
@@ -1,4 +1,6 @@
-﻿namespace DataAccess.Entities
+﻿using System.Collections.Generic;
+
+namespace DataAccess.Entities
 {
     public class OrderItem
     {
@@ -6,5 +8,15 @@
         public int Quantity { get; set; }
         public string SKU { get; set; }
         public int Price { get; set; }
+
+        public List<OrderItem> getAllProducts()
+        {
+            List<OrderItem> lstOrderItem = new List<OrderItem>();
+            lstOrderItem.Add(new OrderItem { Id = 1, SKU = "A", Price = 50 });
+            lstOrderItem.Add(new OrderItem { Id = 2, SKU = "B", Price = 30 });
+            lstOrderItem.Add(new OrderItem { Id = 3, SKU = "C", Price = 20 });
+            lstOrderItem.Add(new OrderItem { Id = 4, SKU = "D", Price = 15 });
+            return lstOrderItem;
+        }
     }
 }
