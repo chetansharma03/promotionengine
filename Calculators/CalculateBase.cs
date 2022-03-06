bfe1936abfdcacfb3c5db5eb6623b6be260cd575
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 12:50:42 +0530

    created calculator base class

---
+++ Calculators/CalculateBase.cs
@@ -0,0 +1,11 @@
+﻿using DataAccess.Entities;
+using PromotionEngine.Data;
+using System.Collections.Generic;
+
+namespace PromotionEngine.Calculators
+{
+    public abstract class CalculateBase
+    {
+        public abstract CheckoutSummary Calculate(CheckoutSummary checkoutSummary, List<OrderItem> orderItems);
+    }
+}
