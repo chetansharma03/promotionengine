17c41a983463fafa2b8642daa3c771e2f4afb8f0
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 12:48:11 +0530

    class is handing selected bundle & single items

---
+++ Data/CheckoutSummary.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PromotionEngine.Data
+{
+    public class CheckoutSummary
+    {
+        public List<BundleItem> BundleItems { get; set; }
+        public List<SingleItem> SingleItems { get; set; }
+        public CheckoutSummary()
+        {
+            BundleItems = new List<BundleItem>();
+            SingleItems = new List<SingleItem>();
+        }
+    }
+}
