cb6983a83a3d09cdf1b8b2ec6ee2ad77b64a8d8e
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 14:39:14 +0530

    interface to calculate discount

---
+++ Interface/ICalculateDiscountRule.cs
@@ -0,0 +1,10 @@
+﻿using DataAccess.Entities;
+using PromotionEngine.Data;
+
+namespace PromotionEngine.Interface
+{
+    public interface ICalculateDiscountRule
+    {
+        BundleItem CalculateDiscount(AnalizeOrderItems rules, Promotion promotion);
+    }
+}
