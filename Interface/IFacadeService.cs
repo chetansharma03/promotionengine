ce6c78a20fc6e134ef9c7be8bf48760d1d5c4774
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 20:55:38 +0530

    user interface

---
+++ Interface/IFacadeService.cs
@@ -0,0 +1,14 @@
+﻿using PromotionEngine.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Interface
+{
+    public interface IFacadeService
+    {
+        string CalculateOrder(Order order);
+    }
+}
