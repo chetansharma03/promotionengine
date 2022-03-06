e0441763c600e0f25732094e0812705978fd7e7a
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 17:55:40 +0530

    default and promotion calculator

---
+++ Calculators/PromotionCalculator.cs
@@ -0,0 +1,44 @@
+﻿using DataAccess.Entities;
+using PromotionEngine.Data;
+using PromotionEngine.Interface;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PromotionEngine.Calculators
+{
+    public class PromotionCalculator : PromotionBase
+    {
+        private readonly Promotion promotion;
+        private readonly ICalculateDiscountRule _discountService;
+        private readonly ICalculateBusinessRule _calcultationBusinessLogic;
+
+        public PromotionCalculator(Promotion promotion, ICalculateBusinessRule calculationBusinessLogic, ICalculateDiscountRule discountServices) : base(promotion, calculationBusinessLogic, discountServices)
+        {
+            this.promotion = promotion;
+            _calcultationBusinessLogic = calculationBusinessLogic;
+            _discountService = discountServices;
+        }
+
+        public override CheckoutSummary Calculate(CheckoutSummary checkoutSummary, List<OrderItem> orderItems)
+        {
+            if (!_calcultationBusinessLogic.ValidateOrder(orderItems, promotion))
+            {
+                return checkoutSummary;
+            }
+
+            var rulesDTO = _calcultationBusinessLogic.ApplyBusinessRules(orderItems, promotion);
+
+            if (rulesDTO.ItemForProccessing.Any())
+            {
+                checkoutSummary.BundleItems.Add(_discountService.CalculateDiscount(rulesDTO, promotion));
+            }
+
+            if (rulesDTO.SingleItems.Any())
+            {
+                checkoutSummary.SingleItems.AddRange(rulesDTO.SingleItems);
+            }
+
+            return checkoutSummary;
+        }
+    }
+}
