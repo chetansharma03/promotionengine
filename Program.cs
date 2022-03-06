b4b51ffdae407d684b1ab514ed60f03b9a28234e
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 07:58:12 +0530

    just displaying list of products and associated promotions

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,39 @@ namespace PromotionEngine
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Program.viewProducts());
+            Console.WriteLine(Program.ViewPromotions());
+            
+
+            Console.ReadLine();
+        }
+
+        public static string ViewPromotions()
+        {
+            Promotion obj = new Promotion();
+            var promotions = obj.Promotions().ToList();
+            string promotionStr = promotions.Any() ? "All Promotions:" + Environment.NewLine : string.Empty;
+
+            foreach (var promotion in promotions)
+            {
+                promotionStr += promotion.Title + Environment.NewLine;
+            }
+
+            return promotionStr;
+        }
+
+        public static string viewProducts()
+        {
+            OrderItem obj = new OrderItem();
+            var products = obj.getAllProducts().ToList();
+            string productsStr = products.Any() ? "All Products:" + Environment.NewLine : string.Empty;
+
+            foreach (var product in products)
+            {
+                productsStr += product.SKU + "- Price:" + product.Price  + Environment.NewLine;
+            }
+
+            return productsStr;
         }
     }
 }
