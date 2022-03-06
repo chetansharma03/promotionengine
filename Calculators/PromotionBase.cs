597e9928844be5b8fce953df71d7fe03aec71672
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 14:40:59 +0530

    collecting promotion, business and discount rules for calculation

---
+++ Calculators/PromotionBase.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Entities;
+using PromotionEngine.Interface;
+
+namespace PromotionEngine.Calculators
+{
+    public abstract class PromotionBase : CalculateBase
+    {
+        private Promotion promotion;
+        public PromotionBase(Promotion promotion, ICalculateBusinessRule calculationBusinessLogic, ICalculateDiscountRule discountServices)
+        {
+            this.promotion = promotion;
+        }
+    }
+}
