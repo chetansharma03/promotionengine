6f42e741f5bec2d91e7e4dc0d53cf0d2df159deb
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 07:43:48 +0530

    modified promotion class, added in memory promotion data

--- Entities/Promotion.cs
+++ Entities/Promotion.cs
@@ -13,5 +13,40 @@ namespace DataAccess.Entities
         public int Quantity { get; set; }
         public DiscountType DiscountType { get; set; }
         public double FixedPriceDiscount { get; set; }
+
+        public double PercentageDiscount { get; set; }
+
+        public List<Promotion> Promotions()
+        {
+            List<Promotion> lstPromotion = new List<Promotion>();
+
+            lstPromotion.Add(new Promotion { BundleType = BundleType.Multiple, DiscountType= DiscountType.FixedPrice
+                             , FixedPriceDiscount=130, Id=1, Quantity=3, SKU="A", Title="3 A's for 130"});
+
+            lstPromotion.Add(new Promotion
+            {
+                BundleType = BundleType.Multiple,
+                DiscountType = DiscountType.Percentage
+                                 ,
+                PercentageDiscount = 20,
+                Id = 2,
+                Quantity = 2,
+                SKU = "B",
+                Title = "3 B's for 48"
+            });
+
+            lstPromotion.Add(new Promotion
+            {
+                BundleType = BundleType.Bundle,
+                DiscountType = DiscountType.FixedPrice
+                             ,
+                FixedPriceDiscount = 30,
+                Id = 3,
+                SKUs = new List<string> { "C","D" },
+                Title = "C and D for 30"
+            }) ;
+
+            return lstPromotion;
+        }
     }
 }
