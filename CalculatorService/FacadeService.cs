c9bc82920077cef098ba94c311a049a4ac247cc0
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 21:03:57 +0530

    this class is hiding actual implementation and drive

---
+++ CalculatorService/FacadeService.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Entities;
+using PromotionEngine.Data;
+using PromotionEngine.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Calculator
+{
+    public class FacadeService : IFacadeService
+    {
+        public string CalculateOrder(Order order)
+        {
+            CalculateService _calculateService = new CalculateService();
+            Promotion obj = new Promotion();
+
+            var checkoutSummary = _calculateService.CalcualteOrder(order, obj.Promotions().ToList());
+
+            var displayResult = string.Empty;
+
+            Console.WriteLine(Environment.NewLine);
+
+            var totalSum = checkoutSummary.SingleItems.Sum(x => x.TotalPrice) +
+                checkoutSummary.BundleItems.Sum(x => x.Amount);
+
+            var totaldiscount = checkoutSummary.BundleItems.Sum(x => x.PromotionDiscount);
+
+            Console.WriteLine($"Promotion discount:{ totaldiscount }");
+            Console.WriteLine($"Total Amount:{ totalSum }");
+
+            return displayResult;
+        }
+    }
+}
