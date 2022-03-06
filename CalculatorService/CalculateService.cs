725b4ae83b30bc7762fc006153ff59247bc81993
chetansharma03 <53168784+chetansharma03@users.noreply.github.com>
2022-03-06 18:07:29 +0530

    calculate promotion and business logic

---
+++ CalculatorService/CalculateService.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Entities;
+using PromotionEngine.CalculatorService;
+using PromotionEngine.Data;
+using PromotionEngine.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Calculator
+{
+    public class CalculateService : ICalculate
+    {
+        public CheckoutSummary CalcualteOrder(Order order, List<Promotion> promotions)
+        {
+            var checkoutSummary = new CheckoutSummary();
+            CalculatorTypeService _calculatorTypeService = new CalculatorTypeService();
+
+            foreach (var promotion in promotions)
+            {
+                checkoutSummary = _calculatorTypeService.GetCalculatorType(promotion).Calculate(checkoutSummary, order.Items);
+            }
+
+            checkoutSummary = _calculatorTypeService.GetDefaultCalculator(promotions).Calculate(checkoutSummary, order.Items);
+
+            return checkoutSummary;
+        }
+    }
+}
---
+++ CalculatorService/CalculatorTypeService.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Entities;
+using PromotionEngine.Calculator;
+using PromotionEngine.Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.CalculatorService
+{
+   public class CalculatorTypeService
+    {
+        public CalculateBase GetCalculatorType(Promotion promotion)
+        {
+            if (promotion.BundleType == DataAccess.Enums.BundleType.Bundle)
+            {
+                return new PromotionCalculator(promotion, new BundleBusinessRules(), new BundleDiscount());
+            }
+            else
+            {
+                return new PromotionCalculator(promotion, new MultipleBusinessRules(), new MultipleDiscount());
+            }
+        }
+
+        public DefaultBase GetDefaultCalculator(List<Promotion> promotions)
+        {
+            return new DefaultCalculator(promotions);
+        }
+    }
+}
